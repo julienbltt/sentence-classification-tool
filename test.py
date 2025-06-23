b71b3ac3f5f5e9df4afe153061e32178f3566965
Julien Balderiotti <julien.balderiotti@ik.me>
2025-06-23 12:29:37 +0100

    Adding a exiting case in test script

--- test.py
+++ test.py
@@ -3,5 +3,10 @@ from sbertClassification import classify
 if __name__ == "__main__":
     while True:
         text = input("Enter your message: ")
+
+        if text.lower() in ['exit', 'quit', 'q']:
+            print("Exiting the program.")
+            break
+
         intent, confidence = classify(text)
         print(f"Predicted Intent: {intent} (confidence={confidence:.2f})")
